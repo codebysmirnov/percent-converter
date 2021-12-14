3701de9d1ec8711b9fc83752d749e61877e97307
Artem Smirnov <codebysmirnov@gmail.com>
2021-12-14 23:23:12 +0300

    Add user retries on first number input
    if the user entered:
    - zero or less;
    - the maximum possible number (determined by the OS).

--- percent_converter.c
+++ percent_converter.c
@@ -4,7 +4,7 @@
 
 /*
     Program: Percent converter
-    Version: 0.2
+    Version: 0.3
 */
 
 // ascii number increase
@@ -17,39 +17,70 @@ int int_from_char(int charachter) {
 
 // checks the possibility of increasing the current number
 int can_it_be_increased(uint64_t a, int b) {
-    uint64_t until_max = ULLONG_MAX - a;
+    uint64_t until_max = ULLONG_MAX - (a * 10);
     if (b >= until_max) {
         return 0;
     }
     return 1;
 }
 
+// error code: when added zero value or less ( <= 0)
+const int err_num_too_small = -1;
+// error code: when added max uint64 value (> ULLONG_MAX)
+const int err_num_too_large = -2;
+
 // return user number input
-uint64_t get_user_input() {
+int get_user_num_input(uint64_t *result) {
     int input = getchar();
-    uint64_t result = 0;
     int char_counter = 1;
 
     while (input != '\n') {
         int number = int_from_char(input);
-        if (!can_it_be_increased(result, number)) {
-            return result;
+        if (number <= 0 && (*result) == 0) {
+            fflush(stdin); // free buffer for drop '\n' from current input
+            return err_num_too_small;
+        }
+        if (!can_it_be_increased(*result, number)) {
+            fflush(stdin); // free buffer for drop '\n' from current input
+            (*result) = 0;
+            return err_num_too_large;
         }
         if (char_counter > 1) {
-            result *= 10;
+            (*result) *= 10;
         }
-        result += number;
+        (*result) += number;
         input = getchar();
         char_counter++;
     }
-    return result;
+    return 0;
 }
 
 int main () {
-    uint64_t first_num;
-    printf("\nUINT_64_T MAX_SIZE: %llu\n", ULLONG_MAX);
-    first_num = get_user_input();
-    printf("\nRESULT :%llu\n", first_num);
+    uint64_t first_num = 0;
+    printf("\nUINT_64_T_MAX_SIZE: %llu\n", ULLONG_MAX);
+
+    int error = 0;
+    error = get_user_num_input(&first_num);
+    while (error < 0) {
+        switch (error) {
+        case err_num_too_small:
+            printf("\nEntered number can't be a zero or less.\n");
+            printf("\nPlease, enter number again on press enter for exit:\n");
+            error = get_user_num_input(&first_num);
+            break;
+        case err_num_too_large:
+            printf("\nMaximum allowed number is: %llu\n", ULLONG_MAX);
+            printf("\nPlease, enter number again on press enter for exit:\n");
+            error = get_user_num_input(&first_num);
+            break;
+        default:
+            printf("UNDEFINED_ERROR_CODE: %d", error);
+            break;
+        }
+    }
+
+    printf("\nRESULT: %llu\n", first_num);
+
     return 0;
 }
 
